0aed75328a3c3f5424486a8c731228b25da90efb
Doublc-Qluv <963880465@qq.com>
2021-01-14 16:12:36 +0800

    inout

---
+++ 1week01/A+B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int n;//循环次数
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int a,b;
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+    return 0;
+    
+}
---
+++ 1week01/cinout.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << n << " hello" << endl;
+    return 0;
+
+}
