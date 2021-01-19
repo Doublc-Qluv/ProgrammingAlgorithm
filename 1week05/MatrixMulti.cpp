45ead730be088a0433f8a22a8d47b126bc7bc0dc
Doublc-Qluv <963880465@qq.com>
2021-01-19 16:12:32 +0800

    matrix

---
+++ 1week05/MatrixMulti.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+#define ROWS 8
+#define LOWS 8
+
+int a[ROWS][LOWS];
+int b[ROWS][LOWS];
+int c[ROWS][LOWS];
+
+int main(){
+    freopen("/Users/doublc.qluv/git_repo/ProgrammingAlgorithm/1week05/matrix_in.txt","r",stdin);
+    
+    int m,n,p,q;
+    //matrix a
+    cin >> m >> n;
+    for(int i = 0; i < m; ++i)
+        for(int j = 0; j < n; ++j)
+            cin >> a[i][j];
+    //matrix b
+    cin >> p >> q;
+    for(int i = 0; i < p; ++i)
+        for(int j = 0; j < q; ++j)
+            cin >> b[i][j];
+
+    //compete c
+    for(int i = 0; i < m; ++i){
+        for(int j = 0; j < q; ++j){
+            c[i][j] = 0;
+            for(int k = 0;k < n; ++k){
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    //out
+    for(int i = 0; i < m; ++i){
+        for(int j = 0; j < q; ++j){
+            cout << c[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
