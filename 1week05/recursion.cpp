4ea27a7c6265f6fca6cad98fbd29d9a609c312bb
Doublc-Qluv <963880465@qq.com>
2021-01-21 17:15:37 +0800

    1w6

--- 1week05/recursion.cpp
+++ 1week05/recursion.cpp
@@ -2,7 +2,7 @@
 #include <cstdio>
 using namespace std;
 
-int factorial(int n){
+int factorial(int n){//阶乘
     if(n < 2){
         return 1;
     }
@@ -12,9 +12,20 @@ int factorial(int n){
     
 }
 
+int fibonacci(int n){//斐波那契数列
+    if(n == 1 || n == 2){
+        return 1;
+    }
+    else{
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+    
+}
+
 int main(){
     int n;
     cin >> n;
-    cout << factorial(n);
+    cout << factorial(n) << endl;
+    cout << fibonacci(n) << endl;
     return 0;
 }
